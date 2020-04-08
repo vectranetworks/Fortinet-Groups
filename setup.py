3d01101cd62b39783d2fc9ee64a7b908eef48c36
Matt Pieklik <mp@vectra.ai>
2020-04-08 09:08:44 -0500

    Minor changes

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='fortinet-groups',
-    description='Fortinet Firewall Cognito Integration',
+    name = 'fortinet-groups',
+    description = 'Fortinet Firewall & Cognito Integration Script',
     version='1.0',
-    packages=['fortinet-groups'],
-    install_requires=['requests', 'pyfortiapi'],
+    url='https://github.com/vectranetworks',
+    keywords="vectra cognito fortinet",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=['requests>=2', 'pyfortiapi'],
+    classifiers=['License :: OSI Approved :: MIT License', 'Programming Language :: Python :: 3']
 )
